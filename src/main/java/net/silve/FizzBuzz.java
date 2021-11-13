308ee15a5a63a5b616e3317f1e5913f76cf6c163
Robert Silve <robert@silve.net>
2021-11-13 09:08:22 +0100

    use Stream

--- src/main/java/net/silve/FizzBuzz.java
+++ src/main/java/net/silve/FizzBuzz.java
@@ -1,26 +1,34 @@
 package net.silve;
 
+import java.util.stream.Stream;
+
 public class FizzBuzz {
 
     private static final String FIZZ = "fizz";
     private static final String BUZZ = "buzz";
 
-    private FizzBuzz() {}
+    private FizzBuzz() {
+    }
 
     public static String[] shout(int max) {
-        String[] fizzbuzzOrNumber = new String[max];
-        for (int i = 0; i < max; i++) {
-            int value = i + 1;
-            fizzbuzzOrNumber[i] = String.valueOf(value);
-            if (value % 3 == 0) {
-                fizzbuzzOrNumber[i] = FIZZ;
-            }
-            if (value % 5 == 0) {
-                if (fizzbuzzOrNumber[i].equals(FIZZ)) {
-                    fizzbuzzOrNumber[i] = fizzbuzzOrNumber[i] + BUZZ;
-                } else {
-                    fizzbuzzOrNumber[i] = BUZZ;
-                }
+        return Stream.iterate(0, i -> i + 1)
+                .limit(max)
+                .map(FizzBuzz::fizzbuzzOrNumber)
+                .toArray(String[]::new);
+    }
+
+    private static String fizzbuzzOrNumber(int i) {
+        String fizzbuzzOrNumber;
+        int value = i + 1;
+        fizzbuzzOrNumber = String.valueOf(value);
+        if (value % 3 == 0) {
+            fizzbuzzOrNumber = FIZZ;
+        }
+        if (value % 5 == 0) {
+            if (fizzbuzzOrNumber.equals(FIZZ)) {
+                fizzbuzzOrNumber = fizzbuzzOrNumber + BUZZ;
+            } else {
+                fizzbuzzOrNumber = BUZZ;
             }
         }
         return fizzbuzzOrNumber;
